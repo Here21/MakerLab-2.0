a143dfc8f31a2f254051e388a9e2f343fbf90b3b
Here21 <deletetmr@gmail.com>
2017-03-06 20:39:45 +0800

    project card

---
+++ imports/ui/components/ProjectCard/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card } from 'antd'
+import './style.css'
+
+const ProjectCard = ({ user }) => (
+  <Card style={{ width: 290, height: 340, margin: '6px 0px' }} bodyStyle={{ padding: 0 }}>
+    <div className="project-card-image">
+      <img alt="example" width="100%" height="208px" src="https://cloud.githubusercontent.com/assets/1698185/18039916/f025c090-6dd9-11e6-9d86-a4d48a1bf049.png" />
+      <p className="project-label">竞赛项目</p>
+    </div>
+    <div className="project-card-content">
+      <h1>Lab名称</h1>
+      <p className="project-introduce">个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述个人描述</p>
+      <div className="project-status-info">
+        <p className="project-author">MakerLab团队</p>
+        <p className="project-viewer">1271</p>
+      </div>
+    </div>
+  </Card>
+)
+
+
+export default ProjectCard
