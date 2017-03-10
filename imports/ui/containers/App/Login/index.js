2673144ceab5f2205f5b51fd101b51efaaeedd33
Here21 <deletetmr@gmail.com>
2017-03-10 22:03:20 +0800

    login

--- imports/ui/containers/App/Login/index.js
+++ imports/ui/containers/App/Login/index.js
@@ -6,12 +6,20 @@ import Registration from '../../../components/RegistrationForm'
 import './style.css';
 
 export default class Login extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      loginState: 'login'
+    }
+  }
   render() {
     return (
       <div className="login-page">
         <div className="inner">
           <Card bordered={false} className="login-page-card">
-            <Registration />
+            {
+              this.state.loginState === 'login' ? <LoginForm /> : <Registration />
+            }
           </Card>
         </div>
       </div>
